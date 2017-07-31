5c9ef921b10e74f1aa822e66644d27de68384058
wfjsw <wfjsw@users.noreply.github.com>
2017-07-31 18:08:27 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+var pkg = require('./package.json');
+console.log(pkg._.name + ' ' + pkg.version);
+
+var argv = require('minimist')(process.argv.slice(2));
+var Bot = require('./lib/libbot.js');
+
+// Default Config
+if(argv._.length == 0) argv._.push('./config.json')
+
+// Load Every Config
+argv._
+    .map(function (config){
+        return require(config);
+    })
+    .forEach(Bot);
